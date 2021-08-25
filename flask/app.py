0228c9a4489d6cbe945875d745543d64911392e7
MINIDELL\marcelo <celo.gomess@gmail.com>
2021-08-24 22:33:11 -0300

    Adicionado o flask e o compose.

---
+++ flask/app.py
@@ -0,0 +1,30 @@
+import os
+import platform
+from time import sleep
+from flask import Flask, abort, url_for, json, jsonify, Response
+import json
+
+app = Flask(__name__,template_folder='.')
+
+os_data = {"System": platform.uname()[0], "Version": platform.uname()[3], "Hostname":platform.node()}
+
+@app.route("/", methods=['GET',])
+def index():
+    return jsonify(os_data)
+
+
+@app.route("/delay", methods=['GET'])
+def delay():
+    sleep(10)
+    return jsonify(os_data)
+
+
+
+@app.route("/bugado", methods=['GET'])
+def bugado():
+    return Response(json.dumps(os_data), status=300, mimetype='application/json')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
